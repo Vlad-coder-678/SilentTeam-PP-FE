fccf56c3a7b8f12ea13ac680c118e086ef86f79c
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-09-03 14:52:18 +0300

    feat: footer component and font Roboto

---
+++ poker/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import LogoRSS from './LogoRSS';
+import f from './Footer.module.scss';
+import FooterTeamLink from './FooterTeamLink';
+
+const teams = [
+  {
+    name: 'anatoliyak',
+    position: 'mentor/teamlead',
+    url: 'https://github.com/anatoliyak',
+  },
+  {
+    name: 'GoodFellow23',
+    position: 'frontend dev',
+    url: 'https://github.com/GoodFellow23',
+  },
+  {
+    name: 'Evgenia-cyber',
+    position: 'fullstack dev',
+    url: 'https://github.com/Evgenia-cyber',
+  },
+  {
+    name: 'Vlad-coder-678',
+    position: 'frontend dev',
+    url: 'https://github.com/Vlad-coder-678',
+  },
+];
+
+const Footer = () => (
+  <div className={f.Footer_wrap}>
+    <div className={f.Footer_item}>
+      Made by
+      {teams.map(({ url, name }) => (
+        <div key={name}>
+          <FooterTeamLink url={url} imgAlt={name} />
+        </div>
+      ))}
+    </div>
+    <div className={f.Footer_item}>
+      from
+      <LogoRSS />
+      in 2021
+    </div>
+  </div>
+);
+
+export default Footer;
