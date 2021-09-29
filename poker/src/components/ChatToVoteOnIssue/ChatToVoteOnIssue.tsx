3d2a53076579c1830e9f2cce736b33c971faae29
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-09-29 23:50:18 +0300

    fix: fix layout on game page and the components present on it

--- poker/src/components/ChatToVoteOnIssue/ChatToVoteOnIssue.tsx
+++ poker/src/components/ChatToVoteOnIssue/ChatToVoteOnIssue.tsx
@@ -1,12 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, Dispatch, SetStateAction } from 'react';
 import { useSelector } from 'react-redux';
-import { issueChatSlice } from '../../redux/slices/gameProcessSlice';
+
 import IssueChatScoreCard from '../IssueChatScoreCard/IssueChatScoreCard';
 import IssueChatUserCard from '../IssueChatUserCard/IssueChatUserCard';
+import ChatCloseButton from '../ChatCloseButton/ChatCloseButton';
+
+import { issueChatSlice } from '../../redux/slices/gameProcessSlice';
 
 import styles from './ChatToVoteOnIssue.module.scss';
 
-const ChatToVoteOnIssue: FC = () => {
+interface Props {
+  isVisible: boolean;
+  setIsVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+const ChatToVoteOnIssue: FC<Props> = ({ isVisible, setIsVisible }) => {
   const issueChat = useSelector(issueChatSlice);
 
   const lastUserRef = React.useRef() as React.MutableRefObject<HTMLDivElement>;
@@ -17,20 +25,21 @@ const ChatToVoteOnIssue: FC = () => {
 
   return (
     <div className={styles.Chat_wrap}>
+      <ChatCloseButton isVisible={isVisible} setIsVisible={setIsVisible} />
       <div className={styles.Chat_cards}>
         {issueChat.length > 0
-        && issueChat.map((item, index) => (
-          <div className={styles.Chat_card} key={item.userId + index}>
-            <IssueChatScoreCard value={item.value} />
-            <IssueChatUserCard
-              userId={item.userId}
-              firstName={item.firstName}
-              lastName={item.lastName}
-              role={item.role}
-              job={item.job}
-            />
-          </div>
-        ))}
+          && issueChat.map((item, index) => (
+            <div className={styles.Chat_card} key={item.userId + index}>
+              <IssueChatScoreCard value={item.value} />
+              <IssueChatUserCard
+                userId={item.userId}
+                firstName={item.firstName}
+                lastName={item.lastName}
+                role={item.role}
+                job={item.job}
+              />
+            </div>
+          ))}
         <div ref={lastUserRef}></div>
       </div>
     </div>
