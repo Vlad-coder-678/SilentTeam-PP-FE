6bfc0a57699184ed0e2b339e0695aeb3e589a6e3
Evgenia Kurineva <kurineva85@mail.ru>
2021-09-29 16:24:51 +0300

    refactor: isAllUsersVoted function

--- poker/src/utils/isAllUsersVoted.ts
+++ poker/src/utils/isAllUsersVoted.ts
@@ -1,8 +1,5 @@
 import { IssueChatItem } from '../types/common';
 
-const isAllUsersVoted = (array: Array<IssueChatItem>): boolean => {
-  const index = array.findIndex((el) => el.value === '-');
-  return index === -1;
-};
+const isAllUsersVoted = (array: Array<IssueChatItem>): boolean => array.every((el) => el.value !== '-');
 
 export default isAllUsersVoted;
